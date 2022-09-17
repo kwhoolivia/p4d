cffa84f1ab9c4286c91e61b76f1ad70115588945
kwhoolivia <113780948+kwhoolivia@users.noreply.github.com>
2022-09-18 00:26:21 +1000

    Update week4.js

--- week4.js
+++ week4.js
@@ -1,3 +1,44 @@
-/*
-    Paste the code for your week 4 exercise below.
-*/
+let midx= 250
+let midy=250
+function setup() {
+  createCanvas(500, 500);
+}
+
+ function draw () {
+  strokeWeight(0)
+   circle(midx, midy, 300);
+    fill ( 48,125,126);
+
+   circle(midx, midy, 250)
+  fill (68,86,68) 
+  
+   fill(56,103,83)
+   rect(midx-70, midy-70, 15,150); 
+   rect(midx+60, midy-70, 15,150);
+   
+   circle (400,350,150)
+   fill  (138,154,97)
+   
+    circle (400,350,110)
+   fill  (138,154,97)
+  fill (29,117,79)
+ textSize(150);
+   textWrap(WORD);
+text('B', 215, 195, 500);
+  
+   textFont('Georgia');
+   fill (30,176,135)
+   textSize(150);
+   textWrap(WORD);
+text('B', 205, 190, 500);
+  
+   textFont('Georgia');
+  
+   fill(61,71,34)
+    rect (420,315,7,70)
+   
+   fill(61,71,34)
+    rect (370,315,7,70)
+   
+   fill (55,65,31)
+}
